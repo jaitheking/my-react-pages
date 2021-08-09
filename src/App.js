ab81ba64fef1586962cd595f36bb00ab9d860df5
Jaiprashanth <jaitheking4858@gmail.com>
2021-08-09 10:32:23 +0800

    Remove workflows

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import 'materialize-css/dist/css/materialize.min.css';
 import 'materialize-css/dist/js/materialize.min.js';
 import {BrowserRouter as Router,Route} from 'react-router-dom';
 import Home from "./components/pages/Home";
-import "./App.css"
+import "./App.css";
 
 function App() {
   return (
     <div className="wrap-content">
       <div className="container">
-        <Router>
+        <Router basename="/my-react-pages">
           <Route exact path="/" component={Home}/>
         </Router>
       </div>
